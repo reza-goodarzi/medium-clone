b8fd7f394073575d6206c2c427235f5d98e135b1
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-06-03 17:09:39 +0430

    Show post details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typing";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -29,8 +36,51 @@ const Home: NextPage = () => {
       </div>
 
       {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt="main image"
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt="author"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
+
+export async function getServerSideProps() {
+  const query = `*[_type == "post"]{
+    _id,title,slug,description,mainImage,body,author -> {
+    name,image
+  }
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
