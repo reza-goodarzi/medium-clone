fac822073ebb9c4ee02451cd26fe8d370571aaef
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-06-03 14:17:34 +0430

    build header

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,30 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">Medium</span> is place to
+            write, read, and connect
+          </h1>
+          <h2>Discover stories, thinking, and expertise from writers on any topic</h2>
+        </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt="logo"
+        />
+      </div>
+
+      {/* Posts */}
     </div>
   );
 };
